247b21a4198c415cbb2ca36f2e7f67176452844c
poppysmic <karlili.li@gmail.com>
2024-03-13 17:59:26 +0000

    Adding some new parameters in the wandb sweep

--- whisper/training/create_sweep.py
+++ whisper/training/create_sweep.py
@@ -20,12 +20,37 @@ parameters_dict = {
         'min': 0,
         'max': 0.001
         },
-
-    }
-parameters_dict.update({
     'num_train_epochs': {
         'value': 1
+    },
+    'eval_steps': {
+      'min': 10,
+      'max':1000
+    },
+    'max_steps': {
+      'min': 10,
+      'max':5000
+    },
+    'gradient_accumulation_steps':  {
+      'min': 1,
+      'max':10
+    },
+    'per_device_train_batch_size':{
+      'min': 1,
+      'max': 16
+    },
+    'per_device_eval_batch_size':{
+      'min': 1,
+      'max': 16
+    },
+    "generation_max_length":{
+      'min': 20,
+      'max': 100
     }
+}
+
+parameters_dict.update({
+    
 })
 
 sweep_config['metric'] = metric
