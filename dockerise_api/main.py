74176f110761488fa93a6f4d06550080c82eabdd
poppysmic <karlili.li@gmail.com>
2024-03-14 14:33:44 +0000

    Adding docker configuration for apple silicon

--- dockerise_api/main.py
+++ dockerise_api/main.py
@@ -1,15 +1,12 @@
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
 
-
-app = FastAPI(docs_url=None)
-
+# app = FastAPI(docs_url=None)
+app = FastAPI()
 
 @app.get("/status")
 def read_root():
     return {"status": "Running.... Welcome language-x-change"}
+
 
 """ 
 todo:
@@ -25,27 +22,27 @@ from fastapi import File, UploadFile
 from transcription.transcribe import transcribe
 from difflib import SequenceMatcher, Differ
 
+
 @app.post("/transcribe")
 def upload_audio(file: UploadFile = File(...)):
     try:
         contents = file.file.read()
-        with open('/app/upload/'+file.filename, 'wb') as f:
+        with open('/app/upload/' + file.filename, 'wb') as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
 
-
     # just keep it simple and use whisper model for now
     result = transcribe(
         "openai/whisper-large-v3",
         "openai/whisper-large-v3",
-        '/app/upload/'+file.filename)
+        '/app/upload/' + file.filename)
 
     # remove the file when processing is done
     try:
-        os.remove('/app/upload/'+file.filename)
+        os.remove('/app/upload/' + file.filename)
         print(f"File /app/upload/'{file.filename}' has been deleted.")
     except FileNotFoundError:
         print(f"Error: File /app/upload/'{file.filename}' not found.")
@@ -54,7 +51,7 @@ def upload_audio(file: UploadFile = File(...)):
         # "message": f"Successfully processed {file.filename}",
         "fileName": file.filename,
         "transcription": f"{result['text']}"
-        }
+    }
 
 
 @app.post('/compare')
@@ -68,24 +65,22 @@ def diff(input, expected):
     character_by_character_diff = []
 
     for index, char in enumerate(difference):
-        
+
         if "-" in char:
             # print(f"Item '{char}' contains a hyphen.")
 
             key = char.replace("-", "").strip()
             if key in expected:
-                character_by_character_diff.append( {  "expected_char": key, "contains": False})
+                character_by_character_diff.append({"expected_char": key, "contains": False})
 
         elif "+" in char:
             key = char.replace("+", "").strip()
             if key in expected:
-                character_by_character_diff.append( { "expected_char": key, "contains": False})
-        
-        else:
-            # print(f"Item '{char}' does not contain a hyphen.")
-            key = char.strip()
-            character_by_character_diff.append( {  "expected_char": key, "contains":  True })
+                character_by_character_diff.append({"expected_char": key, "contains": False})
 
+        else:
+            key = char.strip()
+            character_by_character_diff.append({"expected_char": key, "contains": True})
 
     return {
         "input": f"{input}",
@@ -93,5 +88,5 @@ def diff(input, expected):
         "match_ratio": f"{match_ratio}",
         "char_by_char_diff": character_by_character_diff,
         # "difference": difference,
-        
-        }
+
+    }
